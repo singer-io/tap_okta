eb866e8d77d29bbfae369c8ce25243d108ce5f80
kdamodaran1 <42029576+kdamodaran1@users.noreply.github.com>
2019-06-10 17:44:30 +0100

    Update __init__.py

--- tap-okta/__init__.py
+++ tap-okta/__init__.py
@@ -7,12 +7,7 @@ import datetime
 from singer import metadata
 
 
-header = {}
-args = {}
 schemas = {}
-schema = {}
-groups_for_user = []
-lst_urs_assign_app = []
 LOGGER = singer.get_logger()
 session = requests.session()
 REQUIRED_CONFIG_KEYS = ['accept', 'content-type', 'Authorization', 'service_url']
@@ -21,7 +16,6 @@ schema_list = ['groups','users','applications']
 
 def header_payload(p_data):
 
-    global header
     header = {
         'accept': p_data['accept'],
         'content-type': p_data['content-type'],
@@ -30,52 +24,40 @@ def header_payload(p_data):
     return header
 
 
-def load_data():
+def load_data(p_data):
     for lst_schema in schema_list:
         if lst_schema == 'users':
-            url = args.config['service_url'] + "users"
-            url_pagination(lst_schema,url)
+            url = p_data['service_url'] + "users"
+            url_pagination(lst_schema,url, p_data)
         elif lst_schema == 'groups':
-            url = args.config['service_url'] + "groups"
-            url_pagination(lst_schema, url)
+            url = p_data['service_url'] + "groups"
+            url_pagination(lst_schema, url, p_data)
         elif lst_schema == 'applications':
-            url = args.config['service_url'] + "apps"
-            url_pagination ('applications' , url )
+            url = p_data['service_url'] + "apps"
+            url_pagination ('applications' , url, p_data )
 
 
-def url_pagination(p_schema, p_url):
+def url_pagination(p_schema, p_url, p_data):
+    groups_for_user = []
+    lst_urs_assign_app = []
+    next_url = p_url
 
-    next = False
-    first = True
-    next_url = ""
-    singer.write_schema(p_schema, schema[p_schema], 'id')
-    while (next or first):
-        if next:
-            response = requests.request("GET", next_url, headers=header)
+    header = header_payload(p_data)
+    singer.write_schema(p_schema, schemas[p_schema], 'id')
+    while (next_url):
+
+        response = requests.request("GET", next_url, headers=header)
+        if (response.status_code == 200):
             response_links = requests.utils.parse_header_links(response.headers['Link'].rstrip('>').replace('>,<', ',<'))
             next_url = ""
+
             for linkobj in response_links:
                 if linkobj['rel'] == 'next':
                     next_url = linkobj['url']
-                    next = True
-                else:
-                    next = False
-        if first:
-            response = requests.request("GET", p_url , headers=header)
-            if (response.status_code == 200):
-                response_links = requests.utils.parse_header_links(response.headers['Link'].rstrip('>').replace('>,<', ',<'))
-                next_url = ""
-                for linkobj in response_links:
-                    if linkobj['rel'] == 'next':
-                        next_url = linkobj['url']
-                        next = True
-                        first = False
-                    else:
-                        first = False
-            else:
-                exit(1)
 
-        data = json.loads(response.text)
+        else:
+            exit(1)
+        data = response.json()
         for record in data:
             if p_schema == 'groups':
                 groups_for_user.append(record['id'])
@@ -83,28 +65,27 @@ def url_pagination(p_schema, p_url):
                 lst_urs_assign_app.append(record['id'])
             singer.write_record(p_schema, record)
     if p_schema == 'groups':
-        get_groups_for_user(groups_for_user)
+        get_groups_for_user(groups_for_user, p_data)
     if p_schema == 'applications':
-        list_assigned_groups_app(lst_urs_assign_app)
-        list_users_assigned_to_app(lst_urs_assign_app)
+        list_assigned_groups_app(lst_urs_assign_app, p_data)
+        list_users_assigned_to_app(lst_urs_assign_app, p_data)
 
-
-def get_groups_for_user(p_groups_for_user):
+def get_groups_for_user(p_groups_for_user,p_data):
     for grp_list in p_groups_for_user:
-        p_get_group_user_url = args.config['service_url'] + "groups/" + grp_list + "/users"
-        url_pagination('group_users' , p_get_group_user_url )
+        p_get_group_user_url = p_data['service_url'] + "groups/" + grp_list + "/users"
+        url_pagination('group_users' , p_get_group_user_url, p_data )
 
 
-def list_users_assigned_to_app(p_lst_urs_assign_app):
+def list_users_assigned_to_app(p_lst_urs_assign_app,p_data):
     for usr_app_list in p_lst_urs_assign_app:
-        p_url = args.config['service_url'] + "apps/" + usr_app_list + "/users"
-        url_pagination('application_users' , p_url)
+        p_url = p_data['service_url'] + "apps/" + usr_app_list + "/users"
+        url_pagination('application_users' , p_url, p_data)
 
 
-def list_assigned_groups_app(p_lst_urs_assign_app):
+def list_assigned_groups_app(p_lst_urs_assign_app,p_data):
     for usr_grp_list in p_lst_urs_assign_app:
-        appid_groups_url = args.config['service_url'] + "apps/" + usr_grp_list + "/groups"
-        url_pagination('application_groups' ,appid_groups_url )
+        appid_groups_url = p_data['service_url'] + "apps/" + usr_grp_list + "/groups"
+        url_pagination('application_groups' ,appid_groups_url, p_data )
 
 
 def get_abs_path(path):
@@ -112,9 +93,9 @@ def get_abs_path(path):
 
 
 def load_schemas():
-
-    for filename in os.listdir(get_abs_path('schemas')):
-        path = get_abs_path('schemas') + '\\' + filename
+    global schemas
+    for filename in os.listdir(get_abs_path('tap_okta\schemas')):
+        path = get_abs_path('tap_okta\schemas') + '/' + filename
         file_raw = filename.replace('.json', '')
         with open(path) as file:
             schemas[file_raw] = json.load(file)
@@ -123,14 +104,11 @@ def load_schemas():
 
 def main():
 
-    global args
-    global schema
     args = singer.utils.parse_args(REQUIRED_CONFIG_KEYS)
-
     if args.config:
-        schema = load_schemas()
-        header_payload(args.config)
-        load_data()
+        load_schemas()
+        load_data(args.config)
+
     LOGGER.info('End Date Time : %s', datetime.datetime.now())
 
         
